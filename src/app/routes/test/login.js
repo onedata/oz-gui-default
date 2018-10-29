e97ad35bcceae1a56842d9ee231e25230220e953
Michal Borzecki <michal.borzecki@hotmail.com>
2018-10-29 14:28:26 +0100

    VFS-5013 removed redirect to main page if user is authenticated in test login

--- src/app/routes/test/login.js
+++ src/app/routes/test/login.js
@@ -1,1 +1,16 @@
-export { default } from 'oz-worker-gui/routes/home/login';
+/**
+ * A login test page. Is available regardless user authentication state.
+ * @module routes/home/login
+ * @author Michal Borzecki
+ * @copyright (C) 2018 ACK CYFRONET AGH
+ * @license This software is released under the MIT license cited in 'LICENSE.txt'.
+ */
+
+import LoginRoute from 'oz-worker-gui/routes/home/login';
+
+export default LoginRoute.extend({
+  /**
+   * @override
+   */
+  redirectIfAuthenticated: false,
+});
